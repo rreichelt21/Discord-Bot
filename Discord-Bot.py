d4ec9a3de25fa712031ba1dbf134a42a5424d66d
Ryan Reichelt <53358762+rreichelt21@users.noreply.github.com>
2022-09-04 06:20:02 -0700

    Update Discord-Bot.py

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -8,7 +8,7 @@ client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
