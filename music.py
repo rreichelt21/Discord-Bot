8162ef6281fe44438b1da467ce1806ab4a4f0dcc
Ryan <rreichelt.2002@gmail.com>
2022-09-05 20:24:47 -0700

    Update music.py

--- music.py
+++ music.py
@@ -3,20 +3,18 @@ from discord.ext import commands
 import youtube_dl
 import os
 
-client = commands.Bot(command_prefix = '/', intents=discord.Intents.all())
+client = commands.Bot(command_prefix="/")
 
 @client.command()
 async def play(ctx, url : str):
-    
-    song_exists = os.path.isfile("song.mp3")
-    
+    song_there = os.path.isfile("song.mp3")
     try:
-        if song_exists:
+        if song_there:
             os.remove("song.mp3")
     except PermissionError:
         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
         return
-    
+
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
     await voiceChannel.connect()
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
@@ -31,46 +29,40 @@ async def play(ctx, url : str):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-
     for file in os.listdir("./"):
         if file.endswith(".mp3"):
             os.rename(file, "song.mp3")
-
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
 
 @client.command()
 async def leave(ctx):
-    
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    
     if voice.is_connected():
         await voice.disconnect()
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
+
 @client.command()
 async def pause(ctx):
-
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
     if voice.is_playing():
         voice.pause()
     else:
-        await ctx.send("The bot is not playing anything.")
+        await ctx.send("Currently no audio is playing.")
+
 
 @client.command()
 async def resume(ctx):
-    
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    
     if voice.is_paused():
         voice.resume()
     else:
-        await ctx.send("Music is not paused.")
+        await ctx.send("The audio is not paused.")
+
 
 @client.command()
 async def stop(ctx):
-    
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    
     voice.stop()
